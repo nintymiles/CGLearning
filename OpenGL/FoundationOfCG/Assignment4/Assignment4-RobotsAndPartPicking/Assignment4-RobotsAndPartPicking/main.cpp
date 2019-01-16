3cc726b4b9c37b283baf2de905769bc0228e1c0f
SeanRen <nintymiles@icloud.com>
2019-01-16 23:04:11 +0800

    create empty desktop assignment 4 project

---
+++ OpenGL/FoundationOfCG/Assignment4/Assignment4-RobotsAndPartPicking/Assignment4-RobotsAndPartPicking/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment4-RobotsAndPartPicking
+//
+//  Created by SeanRen on 2019/1/16.
+//  Copyright © 2019 zxtech. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
